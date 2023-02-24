fac37b48e7ae754369ff14f35787671b4153089c
CamilleMaCIS <mehawesomegirl@gmail.com>
2023-02-24 11:53:25 +0800

    Added Song.java

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,39 @@
-public class Song {
+import java.util.ArrayList;
 
-    private String
+public class Song {
+    //properties
+    private String artist;
+    private String title;
+    private ArrayList<String> allStudents;
+
+    //constructor
+    public Song(String title, String artist){
+        this.artist = artist;
+        this.title = title;
+        this.allStudents = new ArrayList<>();
+    }
+
+    //methods
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int howMany(ArrayList<String> todayStudents){
+        int todayNewAmount = 0;
+
+        for (String student : todayStudents){
+            String lowerCaseStudent = student.toLowerCase();
+            if (!this.allStudents.contains(lowerCaseStudent)){
+                this.allStudents.add(lowerCaseStudent);
+                todayNewAmount++;
+            }
+        }
+
+        return todayNewAmount;
+    }
 }
